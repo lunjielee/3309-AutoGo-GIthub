386c732a3d7eabb06686d027afa50275ee2a495b
Brianna0510 <briannawangchen01@gmail.com>
2021-12-05 16:18:41 -0500

    err

--- src/components/staff_home.js
+++ src/components/staff_home.js
@@ -1,31 +1,37 @@
 import React, { useState } from "react";
 
 export default function StaffHome() {
+  const a_style = {
+    backgroundColor: "black",
+    color: "white",
+    textDecoration: "none",
+    padding: "10px",
+    borderRadius: "10px",
+  };
 
-    const a_style = {
-        backgroundColor: "black",
-        color: "white",
-        textDecoration: "none",
-        padding: "10px",
-        borderRadius: "10px",
-    }
+  const a_div_style = {
+    margin: "20px",
+  };
 
-    const a_div_style = {
-        margin: "20px",
-    }
-
-    return (
-        <div>StaffHome page<br />
-            <div style={a_div_style}>
-                <a href='/staff-view-appointment' style={a_style}>view my appointments</a>
-            </div>
-            <div style={a_div_style}>
-                <a href='' style={a_style}>my profile</a>
-            </div>
-            <div style={a_div_style}>
-                <a href='/staff-view-BRevenue' style={a_style}>View Branch Reveneue</a>
-            </div>
-        </div>
-    )
-
+  return (
+    <div>
+      StaffHome page
+      <br />
+      <div style={a_div_style}>
+        <a href="/staff-view-appointment" style={a_style}>
+          view my appointments
+        </a>
+      </div>
+      <div style={a_div_style}>
+        <a href="" style={a_style}>
+          my profile
+        </a>
+      </div>
+      <div style={a_div_style}>
+        <a href="/staff-view-BRevenue" style={a_style}>
+          View Branch Reveneue
+        </a>
+      </div>
+    </div>
+  );
 }
--- src/components/staff_signup.js
+++ src/components/staff_signup.js
@@ -1,65 +1,106 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import Axios from 'axios';
+import Axios from "axios";
 
 export default function StaffSingup() {
-    //React hooks
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-    const [position, setPosition] = useState("");
-    const [branchNo, setBranchNo] = useState("");
-    const API_DOMAIN = process.env.API_DOMAIN || 'localhost';
+  //React hooks
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [position, setPosition] = useState("");
+  const [branchNo, setBranchNo] = useState("");
+  const API_DOMAIN = process.env.API_DOMAIN || "localhost";
 
-    //For navigate between pages
-    let navigate = useNavigate();
+  //For navigate between pages
+  let navigate = useNavigate();
 
-    //Login function
-    const staffSignup = () => {
-        if (username !== '' && password !== '' && branchNo !== '') {
-            console.log('staff-signup usr ' + username)
-            console.log('staff-signup pws ' + password)
-            // Using Axios to send post request to our backend
-            Axios.post(`http://${API_DOMAIN}:8081/api/staff_signup`, {
-                signupType: 'staff',
-                username: username,
-                password: password,
-                position: position,
-                branchNo: branchNo
-            }).then(response => {
-                console.log('Inside')
-                console.log(response)
-                localStorage.setItem("currentUser", username)
-                navigate('/login')
-                window.location.reload();
-            })
-        }
+  //Login function
+  const staffSignup = () => {
+    if (username !== "" && password !== "" && branchNo !== "") {
+      console.log("staff-signup usr " + username);
+      console.log("staff-signup pws " + password);
+      // Using Axios to send post request to our backend
+      Axios.post(`http://${API_DOMAIN}:8081/api/staff_signup`, {
+        signupType: "staff",
+        username: username,
+        password: password,
+        position: position,
+        branchNo: branchNo,
+      }).then((response) => {
+        console.log("Inside");
+        console.log(response);
+        localStorage.setItem("currentUser", username);
+        navigate("/login");
+        window.location.reload();
+      });
     }
-    return (
-        <div>staff signup page</div>,
-
+  };
+  return (
+    (<div>staff signup page</div>),
+    (
+      <div className="signup_con">
         <form>
-            <div class="mb-3">
-                <label for="username" class="form-label">Username</label>
-                <input onChange={(event) => { setUsername(event.target.value) }} class="form-control" id="username" required />
-            </div>
+          <div class="mb-3">
+            <label for="username" class="form-label">
+              Username
+            </label>
+            <input
+              onChange={(event) => {
+                setUsername(event.target.value);
+              }}
+              class="form-control"
+              id="username"
+              required
+            />
+          </div>
 
-            <div class="mb-3">
-                <label for="password" class="form-label">Password</label>
-                <input onChange={(event) => { setPassword(event.target.value) }} type="password" class="form-control" id="password" required />
-            </div>
+          <div class="mb-3">
+            <label for="password" class="form-label">
+              Password
+            </label>
+            <input
+              onChange={(event) => {
+                setPassword(event.target.value);
+              }}
+              type="password"
+              class="form-control"
+              id="password"
+              required
+            />
+          </div>
 
-            <div class="mb-3">
-                <label for="position" class="form-label">Position (optional)</label>
-                <input onChange={(event) => { setPosition(event.target.value) }} class="form-control" id="position" placeholder="E.g. manager" />
-            </div>
+          <div class="mb-3">
+            <label for="position" class="form-label">
+              Position (optional)
+            </label>
+            <input
+              onChange={(event) => {
+                setPosition(event.target.value);
+              }}
+              class="form-control"
+              id="position"
+              placeholder="E.g. manager"
+            />
+          </div>
 
-            <div class="mb-3">
-                <label for="branchNo" class="form-label">Branch Number</label>
-                <input onChange={(event) => { setBranchNo(event.target.value) }} class="form-control" id="branchNo" required />
-            </div>
+          <div class="mb-3">
+            <label for="branchNo" class="form-label">
+              Branch Number
+            </label>
+            <input
+              onChange={(event) => {
+                setBranchNo(event.target.value);
+              }}
+              class="form-control"
+              id="branchNo"
+              required
+            />
+          </div>
 
-            <button onClick={staffSignup} type='button' class="btn btn-primary">Signup As Staff</button>
+          <button onClick={staffSignup} type="button" class="btn btn-primary">
+            Signup As Staff
+          </button>
         </form>
-
+      </div>
     )
+  );
 }
